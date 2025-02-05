d6f51eaab2076b04421754b72c8df2b389913d05
Cuonq_291 <nguyenncuongg291@gmail.com>
2025-02-05 22:43:04 +0700

    tgrwtwr

---
+++ HIT-Cafe-main/HIT-Cafe-main/src/repository/IRepository/IProductRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository.IRepository;
+
+import java.util.List;
+import model.Product;
+
+
+public interface IProductRepository {
+    public Product save(Product product);
+    public List<Product> findAllProduct();
+    public Product update(Product product);
+    public void deleteById(Integer id);
+    public List<Product> findAllByCategory(String category);
+    public List<Product> findAllByCategoryAndName(String name,String category);
+    public Product findByName(String name);
+    public void clear();
+}
